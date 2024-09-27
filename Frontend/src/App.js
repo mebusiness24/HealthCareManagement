569e8814d1525c1fb5f3040a852b6c615a6aa363
mebusiness24 <166483620+mebusiness24@users.noreply.github.com>
2024-09-27 13:29:47 +0530

    Added Routes to Existing Componets by Jagadish

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './Styles/Login.css'
 
 import Loginpage from './component/Loginpage';
 import Registerpage from './component/Registerpage';
 import Verficationpage from './component/Verficationpage';
+import PageNotFound from './component/PageNotFound';
 
 
 function App() {
   return (
-    <div className="App">
-      <Loginpage />
-       {/* <Registerpage />  */}
-      {/* <Verficationpage/>  */}
-      
-     
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Loginpage/>} />
+        <Route path="/register" exact element={<RegisterPage/>} />
+        <Route path="/verfication" exact element={<VerificationPage/>} />
+        <Route path="*" element={<PageNotFound/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Frontend/src/component/PageNotFound.js
@@ -0,0 +1,5 @@
+export const PageNotFound = () =>{
+    <center>
+        <h1>404 : Page Not Found...!</h1>
+    </center>
+}
